b2bcf68596e57138e052964dd2945d6b0d9f69c1
luciaoSainz <danielsainz28@gmail.com>
2021-11-08 18:18:21 +0100

    added the first response from get

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ const chreerio = require('cheerio')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.json('Welcome to my CryptoNews Api')
+})
+
+app.get('/news', (req, res) => {
+    axios.get('')
+    .then(())
+})
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
