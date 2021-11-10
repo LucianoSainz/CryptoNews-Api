b38699b3e909e3fa9d714da91311b3514997727e
luciaoSainz <danielsainz28@gmail.com>
2021-11-10 18:36:36 +0100

    added search title, pictures and url

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const chreerio = require('cheerio')
 
 const app = express()
 
+const articles = []
+
 app.get('/', (req, res) => {
     res.json('Welcome to my CryptoNews Api')
 })
@@ -13,8 +15,18 @@ app.get('/news', (req, res) => {
     axios.get('https://cointelegraph.com/')
     .then((response) => {
         const html = response.data
-        console.log(html)
-    })
+        const $ = chreerio.load(html)
+
+        $('a:contains("bitcoin", "ethereum")', html).each(function (){
+            const title = $(this).text()
+            const url = $(this).attr('href')
+            articles.push({
+                title,
+                url
+            })
+        })
+        res.json(articles)
+    }).catch((err) => console.log(err))
 })
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
